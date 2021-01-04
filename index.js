ffba64941debc04a2849284fa2db82fb77a8925e
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-04 14:53:48 -0700

    more tests with 500 error

--- index.js
+++ index.js
@@ -57,9 +57,6 @@ router.post('/send', (req, res, next) => {
   app.get('/', (req, res) => {
     res.send('Welcome to my app');
   })
-  
-
-
 
   app.listen(process.env.PORT || 3002, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
