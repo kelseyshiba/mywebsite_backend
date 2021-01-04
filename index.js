ff4671f660ff75cf8ae796fa03eeb05ccc363b7c
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-03 21:02:38 -0700

    added code to fix 404

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ router.post('/send', (req, res, next) => {
   app.use(express.static(__dirname + '/'));
   app.use(express.json())
   app.use('/', router)
+  app.get('/', (req, res) => {
+    res.send('Welcome to my app');
+  })
 
   app.listen(process.env.PORT || 3002, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
