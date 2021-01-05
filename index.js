b983978a1d9d85510088a06e04156febe637e1bf
Kelsey Shiba <kelsey.shiba@gmail.com>
2021-01-05 15:24:08 -0700

    entered full URL

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ transporter.verify((error, success) => {
   }
 });
 
-router.post('/send', (req, res, next) => {
+router.post('https://shibadeveloper.com/send', (req, res, next) => {
   console.log('hitting send function')
     var name = req.body.name
     var email = req.body.email
